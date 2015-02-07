e5dbfac5b8dd661cc68f2443bb0f137a698f2251
JDGrimes <jdg@codesymphony.co>
2015-02-07 10:41:54 -0500

    Travis: XMLLint: ignore symlinks
    Fixes #5

--- travis/commands.sh
+++ travis/commands.sh
@@ -124,7 +124,7 @@ codesniff-l10n() {
 # Check XML files for syntax errors.
 codesniff-xmllint() {
 	if [[ $TRAVISCI_RUN == codesniff ]]; then
-		xmllint --noout $(find -L "${CODESNIFF_PATH[@]}" \( -name '*.xml' -o -name '*.xml.dist' \))
+		xmllint --noout $(find "${CODESNIFF_PATH[@]}" -type f \( -name '*.xml' -o -name '*.xml.dist' \))
 	else
 		echo 'Not running xmlint.'
 	fi
