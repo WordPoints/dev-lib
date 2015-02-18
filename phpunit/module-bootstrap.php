1da9c7529b4a9c25863429a9c7c8c5356940d626
JDGrimes <jdg@codesymphony.co>
2015-02-18 15:17:17 -0500

    PHPUnit: automatically load uninstaller
    Closes #22

--- phpunit/module-bootstrap.php
+++ phpunit/module-bootstrap.php
@@ -37,6 +37,25 @@ define( 'WORDPOINTS_MODULE_TESTS_DIR', dirname( dirname( dirname( __FILE__ ) ) )
  */
 require_once( dirname( __FILE__ ) . '/functions.php' );
 
+$has_uninstall_tester = is_dir( WORDPOINTS_MODULE_TESTS_DIR .  '/../../vendor/wordpoints/module-uninstall-tester/' );
+
+if ( $has_uninstall_tester ) {
+
+	/**
+	 * The plugin uninstall testing functions.
+	 *
+	 * @since 1.1.0
+	 */
+	require_once( WORDPOINTS_MODULE_TESTS_DIR . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/includes/functions.php' );
+
+	/**
+	 * The WordPoints modules uninstall testing functions.
+	 *
+	 * @since 1.1.0
+	 */
+	require_once( WORDPOINTS_MODULE_TESTS_DIR . '/../../vendor/wordpoints/module-uninstall-tester/includes/functions.php' );
+}
+
 /**
  * The WordPress tests functions.
  *
@@ -57,10 +76,7 @@ if ( file_exists( WORDPOINTS_MODULE_TESTS_DIR . '/includes/functions.php' ) ) {
 	require_once( WORDPOINTS_MODULE_TESTS_DIR . '/includes/functions.php' );
 }
 
-if (
-	! function_exists( 'running_wordpoints_module_uninstall_tests' )
-	|| ! running_wordpoints_module_uninstall_tests()
-) {
+if ( ! $has_uninstall_tester || ! running_wordpoints_module_uninstall_tests() ) {
 
 	// Hook to load WordPoints.
 	tests_add_filter( 'muplugins_loaded', 'wordpointstests_manually_load_plugin' );
@@ -81,6 +97,23 @@ if (
  * @since 1.0.0
  */
 require( getenv( 'WORDPOINTS_TESTS_DIR' ) . '/includes/bootstrap.php' );
+
+if ( $has_uninstall_tester ) {
+
+	/**
+	 * The plugin uninstall testing bootstrap.
+	 *
+	 * @since 1.1.0
+	 */
+	require_once( WORDPOINTS_MODULE_TESTS_DIR . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/bootstrap.php' );
+
+	/**
+	 * The WordPoints modules uninstall testing bootstrap.
+	 *
+	 * @since 1.1.0
+	 */
+	require_once( WORDPOINTS_MODULE_TESTS_DIR . '/../../vendor/wordpoints/module-uninstall-tester/bootstrap.php' );
+}
 
 /**
  * The module's tests bootstrap.
