1ce8f03b53c29eba10b159585e78667405260917
JDGrimes <jdg@codesymphony.co>
2015-02-19 16:14:05 -0500

    Travis: s/GITHUB_TREE/GIT_TREE/
    See #23

--- travis/commands.sh
+++ travis/commands.sh
@@ -23,11 +23,11 @@ setup-composer() {
 install-from-github() {
 
 	local GITHUB_SRC=${1}_GITHUB_SRC
-	local GITHUB_TREE=${1}_GITHUB_TREE
+	local GIT_TREE=${1}_GIT_TREE
 	local DIR=${1}_DIR
 
     mkdir -p "${!DIR}"
-    curl -L "https://github.com/${!GITHUB_SRC}/archive/${!GITHUB_TREE}.tar.gz" \
+    curl -L "https://github.com/${!GITHUB_SRC}/archive/${!GIT_TREE}.tar.gz" \
         | tar xvz --strip-components=1 -C "${!DIR}"
 }
 
