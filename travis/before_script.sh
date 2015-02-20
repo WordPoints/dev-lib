a7a34670ab0c2877716ebc1a012393b79a866737
JDGrimes <jdg@codesymphony.co>
2015-02-20 09:02:47 -0500

    Update WPCS version

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -19,7 +19,7 @@ export PHPCS_GIT_TREE=master
 # WPCS
 export WPCS_DIR=/tmp/wpcs
 export WPCS_GITHUB_SRC=WordPress-Coding-Standards/WordPress-Coding-Standards
-export WPCS_GIT_TREE=923b63a15c4b9a61f08ab059c3f9e7efa85cb31d
+export WPCS_GIT_TREE=c89260745036f1b842a86759efae79b00da2dc82
 export WPCS_STANDARD=$(if [ -e phpcs.ruleset.xml ]; then echo phpcs.ruleset.xml; else echo WordPress; fi)
 
 # WP L10n Validator
