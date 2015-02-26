2a8dc8f7057c3097a1ae3c1a1bd36e4bdabcfc1d
JDGrimes <jdg@codesymphony.co>
2015-02-26 15:34:16 -0500

    L10n Validator: update default cache file paths
    Closes #31

--- bin/init.sh
+++ bin/init.sh
@@ -27,7 +27,6 @@ fi
 if [ ! -e wp-l10n-validator.json ]; then
 	echo Copying L10n Validator config
 	cp dev-lib/l10n-validator/example-config.json wp-l10n-validator.json
-	mkdir -p tools/wp-l10n-validator
 
 	text_domain=$(grep -oh "Text Domain: .*" src/*.php)
 	text_domain=${text_domain#"Text Domain: "}
