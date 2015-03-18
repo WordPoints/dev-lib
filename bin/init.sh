a96af71db7f092f22e41602dc6dfb4a6c23c49a7
JDGrimes <jdg@codesymphony.co>
2015-03-18 18:00:10 -0400

    Update init script to be used with bootstrap
    See #47

--- bin/init.sh
+++ bin/init.sh
@@ -1,50 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Get the project type from .travis.yml
-if [ -e .travis.yml ]; then
-	project_type=$(grep -o "WORDPOINTS_PROJECT_TYPE=.*" .travis.yml)
-	project_type=${project_type#"WORDPOINTS_PROJECT_TYPE="}
+if [ ! -e .wordpoints-dev-lib-config.sh ]; then
+    echo 'No configuration file (.wordpoints-dev-lib-config.sh) found.'
+    echo Assuming default configuration: DEV_LIB_PATH=dev-lib WORDPOINTS_PROJECT_TYPE=module
 fi
-
-# Ask to enter it manually if not found.
-if [[ $project_type == '' ]]; then
-	echo Please enter the project type: module or wordpoints?
-	read project_type
-fi
-
-# Get the path to the dev_lib tool.
-dev_lib_path=$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )
 
 # Copy the Travis CI configuration file. This is copied rather than symlinked because
 # Travis needs to be able to retrieve it from GitHub.
-echo Copying Travis CI config for $project_type
-cp "$dev_lib_path"/travis/"$project_type".yml .travis.yml
+echo Copying Travis CI config for $WORDPOINTS_PROJECT_TYPE
+cp "$DEV_LIB_PATH"/travis/"$WORDPOINTS_PROJECT_TYPE".yml .travis.yml
 
 # Symlink the PHPCS configuration if you want to use PHPCS.
 if [ ! -e phpcs.ruleset.xml ]; then
 	echo Symlinking PHPCS config
-	ln -s "$dev_lib_path"/phpcs/WordPoints/ruleset.xml phpcs.ruleset.xml
+	ln -s "$DEV_LIB_PATH"/phpcs/WordPoints/ruleset.xml phpcs.ruleset.xml
 fi
 
 # Symlink the PHPUnit configuration for PHPUnit testing.
 if [ ! -e phpunit.xml.dist ]; then
 	echo Symlinking PHPUnit config
-	ln -s "$dev_lib_path"/phpunit/"$project_type".xml.dist phpunit.xml.dist
+	ln -s "$DEV_LIB_PATH"/phpunit/"$WORDPOINTS_PROJECT_TYPE".xml.dist phpunit.xml.dist
 fi
 
 # Symlink the Coveralls configuration file if you want to use code coverage.
 if [ ! -e .coveralls.yml ]; then
 	echo Symlinking Coveralls config
-	ln -s "$dev_lib_path"/travis/.coveralls.yml .
+	ln -s "$DEV_LIB_PATH"/travis/.coveralls.yml .
 fi
 
 # Copy the l10n validator configuration file.
 if [ ! -e wp-l10n-validator.json ]; then
 	echo Copying L10n Validator config
-	cp "$dev_lib_path"/l10n-validator/example-config.json wp-l10n-validator.json
 
 	text_domain=$(grep -oh "Text Domain: .*" src/*.php)
 	text_domain=${text_domain#"Text Domain: "}
+	cp "$DEV_LIB_PATH"/l10n-validator/example-config.json wp-l10n-validator.json
 
 	if [[ $text_domain == '' ]]; then
 		echo Please enter the textdomain for your project:
