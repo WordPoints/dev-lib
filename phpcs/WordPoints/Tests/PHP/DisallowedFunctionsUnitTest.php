8cf58a569a6fa4fbd6a42e8f0463af935064ab8f
JDGrimes <jdg@codesymphony.co>
2015-06-13 18:22:26 -0400

    Sniff for disallowed functions
    Fixes #76, #73, #67

---
+++ phpcs/WordPoints/Tests/PHP/DisallowedFunctionsUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Unit test class for the disallowed functions sniff.
+ *
+ * @package WordPoints_Dev_Lib\Tests
+ * @since 2.1.0
+ */
+
+/**
+ * Unit test class for the disallowed functions sniff.
+ *
+ * @since 2.1.0
+ */
+class WordPoints_Tests_PHP_DisallowedFunctionsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * @since 2.1.0
+	 */
+	public function getErrorList() {
+
+		return array(
+			3 => 1,
+			4 => 1,
+			6 => 1,
+			9 => 1,
+			11 => 1,
+		);
+	}
+
+	/**
+	 * @since 2.1.0
+	 */
+	public function getWarningList() {
+		return array();
+	}
+}
+
+// EOF
