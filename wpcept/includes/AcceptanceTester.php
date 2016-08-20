a2db10aea0eecbf80f6b7d466be37fab4993ac62
JDGrimes <jdg@codesymphony.co>
2016-08-20 17:25:04 -0400

    wpcept: Add methods from core
    Fixes #155

--- wpcept/includes/AcceptanceTester.php
+++ wpcept/includes/AcceptanceTester.php
@@ -107,6 +107,22 @@ class AcceptanceTester extends \Codeception\Actor {
 	}
 
 	/**
+	 * Wait for a new rank to be displayed on the screen.
+	 *
+	 * @since 2.4.0
+	 *
+	 * @param string $context The context in which the rank should appear.
+	 * @param int    $timeout The number of seconds to wait before timing out.
+	 */
+	public function waitForNewRank( $context = '', $timeout = null ) {
+
+		$this->waitUntilElementInteractable(
+			"{$context} .wordpoints-rank.new [name=name]"
+			, $timeout
+		);
+	}
+
+	/**
 	 * Asserts that a success message is being displayed.
 	 *
 	 * @since 2.4.0
@@ -207,6 +223,33 @@ class AcceptanceTester extends \Codeception\Actor {
 				$reaction_id
 			)
 		);
+	}
+
+	/**
+	 * Make this a site with some legacy points hooks disabled.
+	 *
+	 * @since 2.4.0
+	 *
+	 * @param array $hooks The list of hook handler ID bases to disable.
+	 */
+	public function haveSiteWithDisabledLegacyPointsHooks( $hooks ) {
+		update_site_option(
+			'wordpoints_legacy_points_hooks_disabled'
+			, $hooks
+		);
+	}
+
+	/**
+	 * Activate a component.
+	 *
+	 * @since 2.4.0
+	 *
+	 * @param string $slug The component slug.
+	 *
+	 * @return bool Whether the component was activated successfully.
+	 */
+	public function hadActivatedComponent( $slug ) {
+		return WordPoints_Components::instance()->activate( $slug );
 	}
 }
 
