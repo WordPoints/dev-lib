b801dcd9ca80cfb22ac66f830ee7b80dad6dbe5c
JDGrimes <jdg@codesymphony.co>
2016-12-24 13:51:30 -0500

    autoloaders: test src/classes directory
    Fixes #206

--- bin/env.sh
+++ bin/env.sh
@@ -12,7 +12,7 @@ export PROJECT_SLUG=$(basename "$(pwd)" | sed 's/^wp-//')
 # Codesniff path
 CODESNIFF_PATH=(. '!' -path "./$DEV_LIB_PATH/*" '!' -path "./vendor/*" '!' -path "./.idea/*" '!' -path "./node_modules/*" '!' -path "*/.git/*")
 CODESNIFF_PATH_PHP=("${CODESNIFF_PATH[@]}" '(' -name '*.php' -o -name '*.inc' ')')
-CODESNIFF_PATH_PHP_AUTOLOADERS=("${CODESNIFF_PATH_PHP[@]}" -path './src/*/classes')
+CODESNIFF_PATH_PHP_AUTOLOADERS=(src -path '*/classes')
 CODESNIFF_PATH_PHP_L10N_VALIDATOR=(src '(' -name '*.php' -o -name '*.inc' ')')
 
 # Codeception requires PHP 5.4+.
