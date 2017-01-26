be6ce8597f5e2a0914f0b2deeeb1375744508bdd
JDGrimes <jdg@codesymphony.co>
2017-01-26 09:33:33 -0500

    phpunit: Add mock integer attribute class
    Needed for WordPoints/dynamic-points#6

---
+++ phpunit/classes/mock/entity/attr/integer.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Mock integer entity attribute class for the PHPUnit tests.
+ *
+ * @package WordPoints_Dev_Lib\PHPUnit
+ * @since 2.6.0
+ */
+
+/**
+ * Mock integer entity attribute class for the PHPUnit tests.
+ *
+ * @since 2.6.0
+ */
+class WordPoints_PHPUnit_Mock_Entity_Attr_Integer
+	extends WordPoints_PHPUnit_Mock_Entity_Attr {
+
+	/**
+	 * @since 2.6.0
+	 */
+	protected $data_type = 'integer';
+}
+
+// EOF
