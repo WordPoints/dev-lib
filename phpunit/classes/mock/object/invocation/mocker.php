590d2f3c0f3cb6cdb4963d81ad14a8b7a495885c
JDGrimes <jdg@codesymphony.co>
2017-04-20 12:02:23 -0400

    phpunit: support willReturn() on PHP 5.2
    Correct previous commit to place the `willReturn()` method in the
    correct class.

--- phpunit/classes/mock/object/invocation/mocker.php
+++ phpunit/classes/mock/object/invocation/mocker.php
@@ -16,16 +16,13 @@ class WordPoints_PHPUnit_Mock_Object_Invocation_Mocker
 	extends PHPUnit_Framework_MockObject_InvocationMocker {
 
 	/**
-	 * Returns an invocation mocker that requires a method should return a value.
-	 *
 	 * @since 2.7.0
-	 *
-	 * @param mixed $value The value that the method should return.
-	 *
-	 * @return PHPUnit_Framework_MockObject_Stub_Return Return value stub.
 	 */
-	public function willReturn( $value ) {
-		return new PHPUnit_Framework_MockObject_Stub_Return( $value );
+	public function expects( PHPUnit_Framework_MockObject_Matcher_Invocation $matcher ) {
+		return new WordPoints_PHPUnit_Mock_Object_Builder_Invocation_Mocker(
+			$this
+			, $matcher
+		);
 	}
 }
 
