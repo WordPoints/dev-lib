d1eae04020b9a5213323788b7aa250a5d691e915
JDGrimes <jdg@codesymphony.co>
2017-08-24 17:12:14 -0400

    phpunit: Support more mock object methods on PHP 5.2
    PHPUnit 3.6 is the latest version that supports PHP 5.2, and it uses
    PHPUnit Mock Objects 1.1, which doesn't support the `with()`,
    `withConsecutive()`, or `willReturnCallback()` methods, which we use. So
    we've added the necessary code to our shim.

--- phpunit/classes/mock/object/builder/invocation/mocker.php
+++ phpunit/classes/mock/object/builder/invocation/mocker.php
@@ -22,10 +22,88 @@ class WordPoints_PHPUnit_Mock_Object_Builder_Invocation_Mocker
 	 *
 	 * @param mixed $value The value that the method should return.
 	 *
-	 * @return PHPUnit_Framework_MockObject_Stub_Return Return value stub.
+	 * @return PHPUnit_Framework_MockObject_Builder_InvocationMocker Return value stub.
 	 */
 	public function willReturn( $value ) {
 		return $this->will( new PHPUnit_Framework_MockObject_Stub_Return( $value ) );
+	}
+
+	/**
+	 * Returns an invocation mocker that requires a method to return a callback value.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param callable $callback The callback that will supply the value the method
+	 *                           should return.
+	 *
+	 * @return PHPUnit_Framework_MockObject_Builder_InvocationMocker Return value stub.
+	 */
+	public function willReturnCallback( $callback ) {
+
+		return $this->will(
+			new PHPUnit_Framework_MockObject_Stub_ReturnCallback(
+				$callback
+			)
+		);
+	}
+
+	/**
+	 * Validate that a parameters matcher can be defined, throw exceptions otherwise.
+	 *
+	 * @throws PHPUnit_Framework_Exception
+	 */
+	private function canDefineParameters() {
+
+		if ( $this->matcher->methodNameMatcher === null ) {
+			throw new PHPUnit_Framework_Exception(
+				'Method name matcher is not defined, cannot define parameter ' .
+				' matcher without one'
+			);
+		}
+
+		if ( $this->matcher->parametersMatcher !== null ) {
+			throw new PHPUnit_Framework_Exception(
+				'Parameter matcher is already defined, cannot redefine'
+			);
+		}
+	}
+
+	/**
+	 * Sets up a matcher that requires a method to be called with certain parameters.
+	 *
+	 * @param mixed ...$argument The parameters.
+	 *
+	 * @return PHPUnit_Framework_MockObject_Builder_InvocationMocker Parameter matcher.
+	 */
+	public function with() {
+
+		$args = func_get_args();
+
+		$this->canDefineParameters();
+
+		$this->matcher->parametersMatcher =
+			new PHPUnit_Framework_MockObject_Matcher_Parameters( $args );
+
+		return $this;
+	}
+
+	/**
+	 * Sets up a matcher that requires a method to be called with certain parameters.
+	 *
+	 * @param mixed ...$argument The sets of consecutive parameters.
+	 *
+	 * @return PHPUnit_Framework_MockObject_Builder_InvocationMocker Parameter matcher.
+	 */
+	public function withConsecutive() {
+
+		$args = func_get_args();
+
+		$this->canDefineParameters();
+
+		$this->matcher->parametersMatcher =
+			new WordPoints_PHPUnit_Mock_Object_Matcher_Parameters_Consecutive( $args );
+
+		return $this;
 	}
 }
 
