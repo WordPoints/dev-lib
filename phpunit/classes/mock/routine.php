afc6e9dab235e67c1782065511814dc932e2ad45
JDGrimes <jdg@codesymphony.co>
2017-08-25 10:11:35 -0400

    phpunit: add mock routine class
    So that we don't have to try to set the protected property using
    reflection, which doesn't work on PHP 5.2.
    
    See WordPoints/wordpoints#404

---
+++ phpunit/classes/mock/routine.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Mock routine class for the PHPUnit tests.
+ *
+ * @package WordPoints_Dev_Lib\PHPUnit
+ * @since 2.7.0
+ */
+
+/**
+ * Mock routine class for the PHPUnit tests.
+ *
+ * @since 2.7.0
+ */
+abstract class WordPoints_PHPUnit_Mock_Routine extends WordPoints_Routine {
+
+	/**
+	 * @since 2.7.0
+	 */
+	public $network_wide = false;
+}
+
+// EOF
