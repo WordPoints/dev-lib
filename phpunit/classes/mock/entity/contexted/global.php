d507ff9a8bb1e1fcbd693a460767666715efde68
JDGrimes <jdg@codesymphony.co>
2017-09-07 09:30:54 -0400

    phpunit: add mock for an entity from the global context
    Needed for tests for WordPoints/wordpoints#563

---
+++ phpunit/classes/mock/entity/contexted/global.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Mock global contexted entity class for the PHPUnit tests.
+ *
+ * @package WordPoints_Dev_Lib\PHPUnit
+ * @since 2.7.0
+ */
+
+/**
+ * Mock global contexted entity class for the PHPUnit tests.
+ *
+ * @since 2.7.0
+ */
+class WordPoints_PHPUnit_Mock_Entity_Contexted_Global
+	extends WordPoints_PHPUnit_Mock_Entity {
+
+	/**
+	 * @since 2.7.0
+	 */
+	protected $context = '';
+}
+
+// EOF
