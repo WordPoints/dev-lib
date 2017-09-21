6d946a5bd58d7b4c5de3da2bc31193ca5e18d0e0
JDGrimes <jdg@codesymphony.co>
2017-09-21 10:34:30 -0400

    phpcs: use WPCS develop

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -5,6 +5,8 @@ export CODESNIFF_PATH=(. '!' -path "./.idea/*" '!' -path "*/.git/*")
 
 wordpoints-dev-lib-config() {
 
+	export WPCS_GIT_TREE=develop
+
 	export CODESNIFF_PATH_PHP_AUTOLOADERS=(bin)
 	export CODESNIFF_PATH_PHP_PHPCS=("${CODESNIFF_PATH_PHP[@]}" '!' -path "./phpcs/WordPoints/Tests/*")
 }
