e807b147035ba5ec6f56a0ecf33743c90a2acd91
JDGrimes <jdg@codesymphony.co>
2017-10-03 17:52:15 -0400

    set-up: use `mkdir -p` when symlinking pre-commit hook for dev-lib
    Avoids this:
    
    > mkdir: .git/modules/dev-lib/hooks: File exists

--- bin/set-up.sh
+++ bin/set-up.sh
@@ -10,7 +10,7 @@ fi
 # Set up the git pre-commit hook for the dev-lib.
 if [ ! -e .git/modules/"$DEV_LIB_PATH"/hooks/pre-commit ]; then
 	echo Symlinking git pre-commit hook for dev-lib
-	mkdir .git/modules/"$DEV_LIB_PATH"/hooks
+	mkdir -p .git/modules/"$DEV_LIB_PATH"/hooks
 	ln -s ../../../../"$DEV_LIB_PATH"/git/pre-commit .git/modules/"$DEV_LIB_PATH"/hooks
 fi
 
