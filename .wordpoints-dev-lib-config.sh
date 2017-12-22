f4d1ff145ded88ea2d518c81a92179bf98b809d4
JDGrimes <jdg@codesymphony.co>
2017-12-22 17:22:39 -0500

    strings: ignore string that needs to be md5() hashed

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -9,6 +9,14 @@ wordpoints-dev-lib-config() {
 
 	export CODESNIFF_PATH_PHP_AUTOLOADERS=(bin)
 	export CODESNIFF_PATH_PHP_PHPCS=("${CODESNIFF_PATH_PHP[@]}" '!' -path "./phpcs/WordPoints/Tests/*")
+
+	# Ignore some strings that are expected.
+	CODESNIFF_IGNORED_STRINGS=(\
+		"${CODESNIFF_IGNORED_STRINGS[@]}" \
+		# Needs to be processed with a non-cryptographic hashing algorithm to match
+		# the value from core (in the Codeception tests loader class).
+		-e 'dashboard_primary_' \
+	)
 }
 
 # EOF
